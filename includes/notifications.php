cbc5026f63bf99a2e9e509a101c5b9b0f6d0817e
TaqiAldenDev <taqidev1@gmail.com>
2025-12-24 23:44:36 +0300

    Delete includes directory

--- includes/notifications.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Function to create notifications for users
-function create_notification($user_id, $message) {
-    global $pdo;
-    $stmt = $pdo->prepare('INSERT INTO notifications (user_id, message) VALUES (?, ?)');
-    $stmt->execute([$user_id, $message]);
-}
